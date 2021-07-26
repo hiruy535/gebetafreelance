52c396927b6233312013d0d9a13133e7a94f7051
hiruy535 <73457435+hiruy535@users.noreply.github.com>
2021-07-26 11:01:55 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", __file__)
+import django
+django.setup()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
