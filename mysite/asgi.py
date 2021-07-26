331a1bbdcdb58ebe667d547fa21040a6481a2252
hiruy535 <73457435+hiruy535@users.noreply.github.com>
2021-07-26 14:56:15 +0300

    Update asgi.py

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -4,5 +4,4 @@ from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
-ASGI_APPLICATION = "routing.application"
 application = get_default_application()
